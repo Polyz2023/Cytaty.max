d92295804aafbdd91ac6c5765a98f050dccee130
MaksStudio <maxrognov23@gmail.com>
2024-04-20 17:25:42 +0300

    new

---
+++ src/components/foot.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="https://chatmax-0gfc.onrender.com/" class="nav-link px-2 text-body-secondary">Перейти на Chat Maximum</a></li> <br>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Получить цитаты</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">2024 MAX</p>
+  </footer>
+</template>
+
+<script>
+export default{
+    data(){
+
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
